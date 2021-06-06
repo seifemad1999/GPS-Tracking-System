212399cf2cf36582012ee0ef65b1d7e389870d01
ToQa-Mohamed <toukamohamed595@gmail.com>
2021-06-06 15:13:03 +0200

    port driver

---
+++ GPS Tracking system/Port.h
@@ -0,0 +1,25 @@
+ /******************************************************************************
+ *
+ * Module: Port
+ *
+ * File Name: Port.h
+ *
+ * Description: Header file for TM4C123GH6PM Microcontroller - Port Driver.
+ *
+ * Author: seif eldeen amr
+ ******************************************************************************/
+
+#ifndef PORT_H
+#define PORT_H
+//function prototypes
+
+ void SW1_Init(void);
+void lcd_and_led_init(void);
+
+
+
+
+
+
+
+#endif /* PORT_H */
