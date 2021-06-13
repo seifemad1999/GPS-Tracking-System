f2a5d678eaa869e4f9d57b3853614a78191f68aa
seifamr573 <67549960+seifamr573@users.noreply.github.com>
2021-06-13 19:03:20 +0200

    Update EEPROM.c

--- GPS Tracking system/EEPROM.c
+++ GPS Tracking system/EEPROM.c
@@ -19,3 +19,22 @@ void EEPROM_init(void)
   while(BIT_IS_SET(EEPROM_EESUPP_REG , 3) || BIT_IS_SET(EEPROM_EESUPP_REG , 2)) {};
   EEPROM_EEPROT_REG &= ~(1<<3);       
 }
+void EEPROM_WriteData(uint16 currentBlock, uint8 currentOffset,sint32 data)
+{
+	EEPROM_EEBLOCK_REG  = currentBlock;
+	EEPROM_EEOFFSET_REG = currentOffset;
+	EEPROM_EERDWR_REG   = data;
+	while(BIT_IS_SET(EEPROM_EEDONE_REG , working)) {};
+}
+
+sint32 EEPROM_ReadData(uint16 currentBlock, uint8 currentOffset)
+{
+  
+        sint32 data;
+	EEPROM_EEBLOCK_REG = currentBlock;
+	EEPROM_EEOFFSET_REG = currentOffset;
+	data = EEPROM_EERDWR_REG;
+	while(BIT_IS_SET(EEPROM_EEDONE_REG , working)) {};
+	return data;
+  
+}
