840057d3c65ea003bf444ac8bb26b22bec9f03fe
hayat-hamza <hayat.hamza.520@gmail.com>
2021-06-13 15:52:12 +0200

    distance function

--- GPS Tracking system/main.c
+++ GPS Tracking system/main.c
@@ -48,41 +48,46 @@ void sysTick_Handler(void)
    //LCD_displayString(buff);
 //}
 
-
-double degtorad(double deg) {
-	return (deg * pi / 180);
+long double toRadians(const long double degree)
+{
+	// cmath library in C++
+	// defines the constant
+	// M_PI as the value of
+	// pi accurate to 1e-30
+	double one_deg = (pi) / 180;
+	return (one_deg * degree);
 }
 
+long double distance(long double lat1, long double long1,
+	long double lat2, long double long2)
+{
+	// Convert the latitudes
+	// and longitudes
+	// from degree to radians.
+	lat1 = toRadians(lat1);
+	long1 = toRadians(long1);
+	lat2 = toRadians(lat2);
+	long2 = toRadians(long2);
 
-double radtodeg(double rad) {
-	return (rad * 180 / pi);
-}
+	// Haversine Formula
+	long double dlong = long2 - long1;
+	long double dlat = lat2 - lat1;
 
-float distance(float lat1, float lon1, float lat2, float lon2, char unit) {
-	double theta, dist;
-	if ((lat1 == lat2) && (lon1 == lon2)) {
-		return 0;
-	}
-	else {
-		theta = lon1 - lon2;
-		dist = sin(degtorad(lat1)) * sin(degtorad(lat2)) + cos(degtorad(lat1)) * cos(degtorad(lat2)) * cos(degtorad(theta));
-		dist = acos(dist);
-		dist = radtodeg(dist);
-		dist = dist * 60 * 1.1515;
-		switch (unit) {
-		case 'M':
-			break;
-		case 'K':
-			dist = dist * 1.609344;
-			break;
-		case 'N':
-			dist = dist * 0.8684;
-		case 'm':
-			dist = (dist * 1.609344) / 1000;
-			break;
-		}
-		return (dist);
-	}
+	long double ans = pow(sin(dlat / 2), 2) +
+		cos(lat1) * cos(lat2) *
+		pow(sin(dlong / 2), 2);
+
+	ans = 2 * asin(sqrt(ans));
+
+	// Radius of Earth in
+	// Kilometers, R = 6371
+	// Use R = 3956 for miles
+	long double R = 6371;
+
+	// Calculate the result
+	ans = ans * R;
+
+	return ans * 1000;
 }
 
 
